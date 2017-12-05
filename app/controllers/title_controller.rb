5a07f9661dc5f1029415695fcd39a6fce9190f50
Mars Howard <marsbhoward@gmail.com>
2017-12-05 18:12:48 +0000

    automatically backed up by learn

--- app/controllers/title_controller.rb
+++ app/controllers/title_controller.rb
@@ -1,3 +1,5 @@
-class TitlesController < ApplicationController
+class ApplicationController < Sinatra::Base
 
+  set :views, Proc.new { File.join(root, "../views/") }
+  register Sinatra::Twitter::Bootstrap::Assets
 end
