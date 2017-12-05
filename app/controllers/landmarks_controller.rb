c4249d1943cf78a431402ed8ec73f82bdbb9acc3
Mars Howard <marsbhoward@gmail.com>
2017-12-05 18:12:32 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,34 @@
 class LandmarksController < ApplicationController
+  require 'pry'
 
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params['landmark'])
+    redirect to "/landmarks/#{@landmark.id }"
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params['landmark'])
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
 end
