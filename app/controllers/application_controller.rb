576c8264026440259e1405419ee6d5e0679a894b
Mars Howard <marsbhoward@gmail.com>
2017-12-05 18:13:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,5 @@
-class LandmarksController < ApplicationController
-  require 'pry'
+class ApplicationController < Sinatra::Base
 
-  get '/landmarks/new' do
-    erb :'landmarks/new'
-  end
-
-  post '/landmarks' do
-    @landmark = Landmark.create(params['landmark'])
-    redirect to "/landmarks/#{@landmark.id }"
-  end
-
-  get '/landmarks' do
-    @landmarks = Landmark.all
-    erb :'landmarks/index'
-  end
-
-  get '/landmarks/:id' do
-    @landmark = Landmark.find(params[:id])
-    erb :'landmarks/show'
-  end
-
-  post '/landmarks/:id' do
-    @landmark = Landmark.find(params[:id])
-    @landmark.update(params['landmark'])
-    @landmark.save
-    redirect to "/landmarks/#{@landmark.id}"
-  end
-
-  get '/landmarks/:id/edit' do
-    @landmark = Landmark.find(params[:id])
-    erb :'landmarks/edit'
-  end
+  set :views, Proc.new { File.join(root, "../views/") }
+  register Sinatra::Twitter::Bootstrap::Assets
 end
